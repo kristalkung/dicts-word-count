a1471eee7e02c8c924e650d2bd742439f4250815
Kristal <kristalkung@gmail.com>
2021-01-29 05:15:50 +0000

    Improved output: removed punctuation and made all words in lower case

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,5 @@
+import pdb
+
 word_file = open("twain.txt")
 
 word_counts = {}
@@ -9,24 +11,34 @@ word_counts = {}
 
 for line in word_file:
     line_list = line.rstrip().split(" ")
-    
+# print(word_counts)
+
     for word in line_list:
+        # print(word)
+        if len(word) == 0:
+            continue
+
+        if word[-1] in "'\",.!?-#$%^&();:_":
+            word = word[:-1]
+        
+        word = word.lower()
         word_counts[word] = word_counts.get(word, 0) + 1
 
 for word, value in word_counts.items():        
     print(f"{word} {word_counts[word]}")
 
+# print(word_counts)
 
+       # define punctuation
 
-       # def make_letter_counts_dict(phrase):
-    #"""Return dict of letters and # of occurrences in phrase."""
+# To take input from the user
+# my_str = input("Enter a string: ")
 
-    #letter_counts = {}
+# remove punctuation from the string
+# no_punct = ""
+# for char in my_str:
+#    if char not in punctuations:
+#        no_punct = no_punct + char
 
-    #for letter in phrase:
-        #letter_counts[letter] = letter_counts.get(letter, 0) + 1
-
-    #return letter_counts
-
-    #for animal, number in animals.items():
-...    #print(f'{animal}: {number}')
+# # display the unpunctuated string
+# print(no_punct)
